349612174f79df6c8edf202833cb500b65bab750
sophy1 <jihye.sophy.won@gmail.com>
2022-10-19 20:09:19 +0000

    Update 3 files

--- src/tutorial/state-and-lifecycle/StateAndLifecycle.js
+++ src/tutorial/state-and-lifecycle/StateAndLifecycle.js
@@ -7,22 +7,30 @@ function getRandomColor() {
   return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 const StateAndLifecycle = () => {
+    // Toggle <Clock>
+    const [enable, setEnable] = useState(true);
+    const onToggleTimer = () => {
+        setEnable(!enable);
+    };
+    
+    // Toggle <LifeCyleSample>
     const [color, setColor] = useState("#000000");
     const [visible, setVisible] = useState(true);
 
     const onClick = () => {
         setColor(getRandomColor());
     };
-
     const onToggle = () => {
         setVisible(!visible);
     };
+    
 
     return (
         <>
           <div> State And Lifecycle </div>
           <hr />
-          <Clock />
+          <button onClick={onToggleTimer}>토글 Timer</button>
+          {enable && <Clock />}
           <hr/>
           <button onClick={onClick}>랜덤 색상</button>
           <button onClick={onToggle}>토글</button>
